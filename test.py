e0bef7c547a0415c43dec25441c3a4e59e744600
poseidon <jsy10303@gmail.com>
2022-11-03 09:34:44 +0900

    p312까지..

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from array import array
-import pyexcel as px
-my_array=[[1,2,3],[1,2,3],[1,2,3],[1,2,3],]
-px.save_as(array=my_array,dest_file_name="test.xlsx")
-# px.save_as(my_array,"test.xlsx")
+import datetime
+
+print(datetime.datetime.today())
+print(str(datetime.datetime.today().date()))
+print(str(datetime.timedelta(days=2)))
